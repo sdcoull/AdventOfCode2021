5eec2466083b3210cd7fab7371a9fd129453a0b7
Steven Coull <steven.c@wonderlabedu.com>
2021-12-02 19:03:29 +0000

    Day 1 - Part 1

--- AdventOfCode2021/main.cpp
+++ AdventOfCode2021/main.cpp
@@ -5,10 +5,26 @@
 //  Created by Steven Coull on 02/12/2021.
 //
 
+using namespace std;
 #include <iostream>
+#include <fstream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, Steven! Starting...\n";
+    
+    int previousMeasurement = -1;
+    int depthIncreaseCounter = 0;
+    string myText;
+    ifstream MyReadFile("Day1Input.txt");
+    
+    while (getline (MyReadFile, myText)) {
+        int depthMeasurement = stoi(myText);
+        if(previousMeasurement != -1 && depthMeasurement > previousMeasurement)
+            depthIncreaseCounter++;
+        previousMeasurement = depthMeasurement;
+    }
+    MyReadFile.close();
+    
+    cout << "Increase Counter: " << depthIncreaseCounter << "\n";
     return 0;
 }
