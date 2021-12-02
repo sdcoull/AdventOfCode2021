1982bd82ecc53855841f095fd60bc02b17062ea0
Steven Coull <steven.c@wonderlabedu.com>
2021-12-02 18:34:57 +0000

    Initial Commit

---
+++ AdventOfCode2021/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AdventOfCode2021
+//
+//  Created by Steven Coull on 02/12/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
